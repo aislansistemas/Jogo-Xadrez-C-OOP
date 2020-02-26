52da688d1dbe6f78444e39524761e306a66f9266
aislan peixoto <aislanhouse10@gmail.com>
2020-02-26 20:57:02 -0300

    criando a classe posição xadrez

---
+++ xadrez/PosicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace jogo_xadrez.xadrez
+{
+    class PosicaoXadrez
+    {
+        private int v1;
+        private int v2;
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+        public PosicaoXadrez(char coluna,int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(coluna - 'a', 8 - linha);
+        }
+        public override string ToString()
+        {
+            return ""+coluna+linha;
+        }
+    }
+}
